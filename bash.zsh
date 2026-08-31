69d9189d3f76ac62b90093694501582c6d14c79f
Seriki Walter Yakub <251400999+auraecosystem@users.noreply.github.com>
2026-09-01 00:57:00 +0100

    Update bash.zsh

--- bash.zsh
+++ bash.zsh
@@ -1,2 +1,6 @@
 $ bazel build @dbx_build_tools//build_tools:bzl
 $ sudo cp -rL bazel-bin/external/dbx_build_tools/build_tools/bzl{,.runfiles} /usr/bin/
+$ bzl itest-start //python/website:website_service
+$ curl http://localhost:5432
+<html><strong>Hello</strong></html>
+$ bzl itest-stop //python/website:website_service
